fb732190c5efa9ea353516b6c3ab4430290e3b5c
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-01-21 18:38:11 +0300

    receive data success

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,61 @@
     windows_subsystem = "windows"
 )]
 
+use std::fs::OpenOptions;
+use std::io::{Write, BufReader, BufRead};
+use std::time::Duration;
+use home::home_dir;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn transfer_data(port: String) -> String {
+    println!("{port}");
+
+    let mut path = home_dir()
+    .expect("Ошибка доступа к домашней директории");
+  path.push("logs.txt");
+  let mut file = OpenOptions::new()
+  .create(true)
+  .append(true)
+  .open(path)
+  .expect("Ошибка при открытии файла");
+
+    let serial_port = serialport::new(port, 9600)
+    .timeout(Duration::from_millis(1000))
+    .open()
+    .expect("Failed to open serial port");
+
+  let mut reader = BufReader::new(serial_port);
+  let mut my_str = String::new();
+
+  loop{
+    my_str.clear();
+    reader.read_line(&mut my_str);
+    my_str.into();
+    writeln!(file, "{my_str}").expect("Ошибка при записи файла");
+  }
+
+}
+
+#[tauri::command]
+fn log(port: String) {
+    println!("{port}");
+  let mut path = home_dir()
+    .expect("Ошибка доступа к домашней директории");
+  path.push("logs.txt");
+
+  let mut file = OpenOptions::new()
+    .create(true)
+    .append(true)
+    .open(path)
+    .expect("Ошибка при открытии файла");
+  writeln!(file, "{port}").expect("Ошибка при записи файла");
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
+      .invoke_handler(tauri::generate_handler![log, transfer_data])
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
+  }
--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { FormEvent, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  const [port, setPort] = useState('')
+  const [dataFromBack, setDataFromBack] = useState<string[]>([])
+  const start = (e: FormEvent) => {
+    e.preventDefault()
+    invoke('transfer_data', { port }).then(res => setDataFromBack(prev => [...prev, res as string]))
+    setPort('')
   }
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
-      </div>
-      <p>{greetMsg}</p>
+      <h1>Welcome to 3D Receiver!</h1>
+      <form id="create-form">
+        <input type="text" id="port" placeholder="Введите порт" value={port} onChange={e => setPort(e.target.value)} />
+        <button onClick={(e) => start(e)} type="submit" className="create-button">Create file</button>
+      </form>
+      <p>{dataFromBack}</p>
     </div>
   );
 }

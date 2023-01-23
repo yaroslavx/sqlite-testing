75b8e1cd43124c9313f154f138a81e4396b4e1a5
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-01-23 13:21:00 +0300

    get rid of warnings

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,7 @@ use std::fs::OpenOptions;
 use std::io::{Write, BufReader, BufRead};
 use std::time::Duration;
 use home::home_dir;
-use tauri::{Manager, Window};
+use tauri::{Window};
 
 #[derive(Clone, serde::Serialize)]
 struct Payload {
@@ -27,7 +27,7 @@ fn init_process(port: String, window: Window) {
       .open(path)
       .expect("Ошибка при открытии файла");
 
-    let mut serial_port = serialport::new(port, 9600)
+    let serial_port = serialport::new(port, 9600)
       .timeout(Duration::from_millis(1000))
       .open()
       .expect("Ошибка при открытии Serial Port");
@@ -42,7 +42,7 @@ fn init_process(port: String, window: Window) {
       //       .write_all("<".as_bytes()).expect("Write failed!");
       // std::thread::sleep(Duration::from_millis(1500));
 
-      port.read_line(&mut my_str);
+      port.read_line(&mut my_str).unwrap();
       window.emit("data", Payload { data: my_str.clone().into() }).unwrap();
       writeln!(file, "{my_str}").expect("Ошибка при записи файла");
     }

ab9c3c0cd20bbabc6958db1e0478da0c454a61bc
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-01-24 23:52:26 +0300

    little changes

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -47,7 +47,9 @@ const Main = () => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.menu}></div>
+            <div className={styles.menu}>
+                <div></div>
+            </div>
             <Plot
                 className={styles.plot}
                 data={[

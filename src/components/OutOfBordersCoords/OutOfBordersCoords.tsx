41d146f96f303c2a79d916af7516fc26e46ac904
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-04-28 13:25:00 +0300

    Update, nothing interesting

--- src/components/OutOfBordersCoords/OutOfBordersCoords.tsx
+++ src/components/OutOfBordersCoords/OutOfBordersCoords.tsx
@@ -7,12 +7,12 @@ type OutOfBordersCoordsProps = {
 
 export const OutOfBordersCoords = ({ min, max }: OutOfBordersCoordsProps) => {
     return <div className="outofborders_container">
-        {min && <div className='outofborders_min'>
+        {min.length > 0 && <div className='outofborders_min'>
             <span className="mincoords_title">Координаты выхода за нижнюю границу:</span>
-            {['1, 2, 3', '4, 5, 6', '7, 8, 9'].map((coord, index) => <div key={coord} className="min_coord">{index}: {'['}{coord}{']'}</div>)}
+            {min.map((coord, index) => <div key={coord} className="min_coord">{index}: {'['}{coord}{']'}</div>)}
         </div>}
-        {max && <div className='outofborders_max'>
+        {max.length > 0 && <div className='outofborders_max'>
             <span className="macoords_title">Координаты выхода за верхнюю границу:</span>
-            {['1, 2, 3', '4, 5, 6', '7, 8, 9'].map((coord, index) => <div key={coord} className="max_coord">{index}: {'['}{coord}{']'}</div>)}</div>}
+            {max.map((coord, index) => <div key={coord} className="max_coord">{index}: {'['}{coord}{']'}</div>)}</div>}
     </div>
 }
